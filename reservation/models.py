493f8598f503f32bad552c322fb29c5b0205c4a0
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 09:24:33 +0430

    feat: create models
    create Hotel and Room and Book models

--- reservation/models.py
+++ reservation/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Hotel(models.Model):
+    name = models.CharField(max_length=1000)
+    address = models.CharField(max_length=3000)
+
+
+class Room(models.Model):
+    ROOM_CATEGORIES = (
+        ('Single','Single'),
+        ('Double','Double')
+    )
+    number = models.IntegerField()
+    hotel = models.ForeignKey(to=Hotel,on_delete=models.CASCADE)
+    category = models.CharField(max_length=6,choices=ROOM_CATEGORIES)
+    capacity  = models.IntegerField()
+    beds = models.IntegerField()
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=200)
+    room = models.ForeignKey(to=Room,on_delete=models.CASCADE)
+    check_in = models.DateTimeField()
+    check_out = models.DateTimeField()
