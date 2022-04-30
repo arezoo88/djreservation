ce8e737f535e044bd8848926d803c87683464738
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 14:55:38 +0430

    fix: edit datetime

--- reservation/utils/availability.py
+++ reservation/utils/availability.py
@@ -5,8 +5,8 @@ def check_availability(room, check_in, check_out):
     available_list = []
     booking_list = Booking.objects.filter(room=room)
     for booking in booking_list:
-        if booking.check_in > check_out or booking.check_out < check_in:
+        if booking.check_in.strftime('%Y-%m-%dT%H:%M') > check_out or booking.check_out.strftime('%Y-%m-%dT%H:%M') < check_in:
             available_list.append(True)
         else:
-            available_list.append(True)
+            available_list.append(False)
     return all(available_list)
