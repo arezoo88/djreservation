c9ef625e842356b588ec3e8a6b7c71c3ad1fbc39
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 09:52:45 +0430

    fix: autopep8
    install autopep8  in vscode and perform that.

--- reservation/admin.py
+++ reservation/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from nested_inline.admin import NestedStackedInline, NestedModelAdmin
 from .models import *
+
+
 class RoomInLine(NestedStackedInline):
     model = Room
     extra = 1
+
+
 @admin.register(Hotel)
 class HotelInline(NestedModelAdmin):
-    model= Hotel
+    model = Hotel
     list_display = ('name', 'address')
     inlines = [
         RoomInLine
--- reservation/models.py
+++ reservation/models.py
@@ -1,28 +1,31 @@
 from django.db import models
 
+
 class Hotel(models.Model):
     name = models.CharField(max_length=1000)
     address = models.CharField(max_length=3000)
+
     def __str__(self):
         return self.name
 
 
 class Room(models.Model):
     ROOM_CATEGORIES = (
-        ('Single','Single'),
-        ('Double','Double')
+        ('Single', 'Single'),
+        ('Double', 'Double')
     )
     number = models.IntegerField()
-    hotel = models.ForeignKey(to=Hotel,on_delete=models.CASCADE)
-    category = models.CharField(max_length=6,choices=ROOM_CATEGORIES)
-    capacity  = models.IntegerField()
+    hotel = models.ForeignKey(to=Hotel, on_delete=models.CASCADE)
+    category = models.CharField(max_length=6, choices=ROOM_CATEGORIES)
+    capacity = models.IntegerField()
     beds = models.IntegerField()
+
     def __str__(self) -> str:
-         return f'{self.number} in {self.hotel.name}'
+        return f'{self.number} in {self.hotel.name}'
 
 
 class Book(models.Model):
     name = models.CharField(max_length=200)
-    room = models.ForeignKey(to=Room,on_delete=models.CASCADE)
+    room = models.ForeignKey(to=Room, on_delete=models.CASCADE)
     check_in = models.DateTimeField()
     check_out = models.DateTimeField()
