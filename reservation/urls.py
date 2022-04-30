1efc65653d28307726a6b4be538e1e67c179c5ba
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 13:40:08 +0430

    feat: add url
    add new path url for booking

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
+from reservation.views import BookingCreateApiView
+
 app_name = 'reservation'
 
 urlpatterns = [
-
+    path('book/', BookingCreateApiView.as_view(), name='BookingApiView')
 ]
