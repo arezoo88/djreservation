1ee79e198f4b25fc1d586b278d57ef0ec682cbec
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 17:46:02 +0430

    feat: new url
    add url booking_list

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from reservation.views import BookingCreateApiView
+from reservation.views import BookingCreateApiView, BookingListApiView
 
 app_name = 'reservation'
 
 urlpatterns = [
-    path('book/', BookingCreateApiView.as_view(), name='BookingApiView')
+    path('book/', BookingCreateApiView.as_view(), name='Booking'),
+    path('booking_list/', BookingListApiView.as_view(), name='BookingList')
 ]
