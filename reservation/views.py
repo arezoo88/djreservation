e132e2efc28b564db92a6104193d2f0ee2c1fdcd
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 15:32:14 +0430

    feat: add condition
    add condition  for checking capacity in BookingCreateApiView

--- reservation/views.py
+++ reservation/views.py
@@ -1,6 +1,8 @@
 from rest_framework import generics
 from rest_framework.response import Response
-from rest_framework.status import HTTP_404_NOT_FOUND
+from rest_framework.status import HTTP_404_NOT_FOUND, HTTP_400_BAD_REQUEST
+from rest_framework.generics import get_object_or_404
+from reservation.models import Booking, Room
 from .utils.availability import check_availability
 from .serializers import BookingSerializer
 
@@ -12,10 +14,15 @@ class BookingCreateApiView(generics.CreateAPIView):
         data = request.data
         serializer = self.get_serializer(data=data)
         serializer.is_valid(raise_exception=True)
-        room_number = data.get('room')
+        room_pk = data.get('room')
+        room_obj = get_object_or_404(Room, pk=room_pk)
+        if 'capacity' not in data: #TODO handle error format
+            return Response({'success': False, 'msg': 'capacity field is mandatory'}, status=HTTP_400_BAD_REQUEST)
         check_in = data.get('check_in')
         check_out = data.get('check_out')
-        if check_availability(room_number, check_in, check_out) == True:
+        if check_availability(room_pk, check_in, check_out) == True:
+            if room_obj.capacity < data.get('capacity'):
+                return Response({'success': False, 'msg': f'maximum capacity is {room_obj.capacity}'}, status=HTTP_400_BAD_REQUEST)
             serializer.save()
             return Response(serializer.data)
         else:
