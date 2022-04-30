4bd5f94c4ad5370b3302694ef31291140ad12c83
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 11:04:26 +0430

    feat: add new app
    add utils folder.

--- djreservation/settings.py
+++ djreservation/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'reservation',
     # third-party packages
-    'nested_inline'
+    'nested_inline',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
