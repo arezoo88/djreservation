0c1602246a9a14fb95d8536f0870513f30f5ecf3
Arezoo <arezoo.darvishi69@yahoo.com>
2022-04-30 14:54:35 +0430

    feat: create BookingCreateApiView
    create api for book room

--- reservation/views.py
+++ reservation/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.status import HTTP_404_NOT_FOUND
+from .utils.availability import check_availability
+from .serializers import BookingSerializer
 
-# Create your views here.
+
+class BookingCreateApiView(generics.CreateAPIView):
+    serializer_class = BookingSerializer
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        serializer = self.get_serializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        room_number = data.get('room')
+        check_in = data.get('check_in')
+        check_out = data.get('check_out')
+        if check_availability(room_number, check_in, check_out) == True:
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            # TODO check status code
+            return Response({'success': False, 'msg': 'this room not available'}, status=HTTP_404_NOT_FOUND)
