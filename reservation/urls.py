fcf735035c4460c58417cf3175c33a38516cc2dc
Arezoo <arezoo.darvishi69@yahoo.com>
2022-05-01 12:05:19 +0430

    fix:

--- reservation/urls.py
+++ reservation/urls.py
@@ -6,5 +6,5 @@ app_name = 'reservation'
 
 urlpatterns = [
     path('book/', BookingCreateApiView.as_view(), name='Booking'),
-    path('booking_list/', BookingListApiView.as_view(), name='BookingList')
+    path('booking_list/', BookingListApiView.as_view({'get': 'list'}), name='BookingList')
 ]
