2977e8ccb139dbe17e2ff00a6c5b57bc8e0731e5
Arezoo <arezoo.darvishi69@yahoo.com>
2022-05-01 16:40:03 +0430

    feat: new serializers
    create serializer for Room and Hotel models

--- reservation/serializers.py
+++ reservation/serializers.py
@@ -1,5 +1,8 @@
+from asyncore import write
+from pyexpat import model
+from pkg_resources import require
 from rest_framework import serializers
-from .models import Booking
+from .models import Booking, Hotel, Room
 
 
 class BookingSerializer(serializers.ModelSerializer):
@@ -26,4 +29,30 @@ class BookingSerializer(serializers.ModelSerializer):
         rep = super(BookingSerializer, self).to_representation(instance)
         rep['room'] = instance.room.number
         rep['capacity'] = instance.room.capacity
+        rep['hotel'] = instance.room.hotel
         return rep
+
+
+class HotelSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hotel
+        fields = ('name', 'address')
+
+
+class RoomSerializer(serializers.ModelSerializer):
+    hotel = HotelSerializer(required=False)
+    check_in = serializers.DateTimeField(required=True, write_only=True)
+    check_out = serializers.DateTimeField(required=True, write_only=True)
+
+    class Meta:
+        model = Room
+        fields = '__all__'
+        extra_kwargs = {
+            "number": {"required": False}, "category": {"required": False}, "capacity": {"required": False}, "beds": {"required": False}
+        }
+
+    def validate(self, data):
+        if data["check_out"] < data["check_in"]:
+            raise serializers.ValidationError({
+                "error": "check_in datetime cannot be greater than check_out datetime",
+            })
