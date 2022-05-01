f0b43f0b1939b5b5f52732e30493ce09ae0ef0e8
Arezoo <arezoo.darvishi69@yahoo.com>
2022-05-01 16:40:48 +0430

    feat: new url
    create url for  getting available rooms

--- reservation/urls.py
+++ reservation/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from reservation.views import BookingCreateApiView, BookingListApiView
+from reservation.views import BookingCreateApiView, BookingListApiView, find_available_rooms
 
 app_name = 'reservation'
 
 urlpatterns = [
     path('book/', BookingCreateApiView.as_view(), name='Booking'),
-    path('booking_list/', BookingListApiView.as_view({'get': 'list'}), name='BookingList')
+    path('booking_list/', BookingListApiView.as_view({'get': 'list'}), name='BookingList'),
+    path('avail_rooms/', find_available_rooms, name='AvailRooms')
 ]
