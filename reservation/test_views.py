e7e8aeb34f924dc56f71bb0d427978e9dd694ffe
Arezoo <arezoo.darvishi69@yahoo.com>
2022-05-03 14:37:27 +0430

    feat: create FindAvailableRoomTest
    create new class for testing find_available_rooms

--- reservation/test_views.py
+++ reservation/test_views.py
@@ -1,8 +1,8 @@
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
-import datetime
 from reservation.models import *
+import json
 
 
 class BookingCreateApiViewTest(APITestCase):
@@ -31,3 +31,19 @@ class BookingCreateApiViewTest(APITestCase):
         response = self.client.post(
             reverse('reservation:Booking'), sample_data)
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class FindAvailableRoomTest(APITestCase):
+    fixtures = ["fixtures/db.json"]
+
+    def test_get_all_of_rooms_when_non_of_them_do_not_reserverd_in_period_of_time(self):
+        # check db in fixtures/db.json (in db there is 7 rooms)
+        response = self.client.get(
+            reverse('reservation:AvailRooms'), {'check_in': '2022-07-27T22:22', 'check_out': '2022-09-30T22:22'})
+        self.assertEqual(len(json.loads(response.content)), 7)
+
+    def test_should_not_get_rooms_that_are_not_available(self):
+        # check db in fixtures/db.json (in db there is 7 rooms but rooms with pk:3,5 in this perid of dates not available)
+        response = self.client.get(
+            reverse('reservation:AvailRooms'), {'check_in': '2022-06-18T22:22', 'check_out': '2022-06-25T22:22'})
+        self.assertEqual(len(json.loads(response.content)), 5)
